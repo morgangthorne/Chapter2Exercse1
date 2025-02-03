09245468f23f31b19ce9772e9737ea96082ca9b3
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-03 08:20:20 -0500

    Added third line

--- Chapter2Exercise1.cpp
+++ Chapter2Exercise1.cpp
@@ -6,7 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Hello World again!/n";
+    std::cout << "Hello World again!\n";
+    std::cout << "Hello World again!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
